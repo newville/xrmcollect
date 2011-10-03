8b50be8dacfd158f85485720d77709eee402c59c
Matt Newville <newville@cars.uchicago.edu>
2011-10-02 19:39:03 -0500

    more reorganization

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,9 +1,9 @@
 __version__ = '0.2'
 
-from .gui.fastmap_gui import FastMapGUI
 from .xmap import read_xmap_netcdf, MultiXMAP, DXP, MCA
 
-import utils
+import .util
 
 from config import FastMapConfig, conf_files, default_conf
 from mapper import mapper
+#from .gui.fastmap_gui import FastMapGUI
