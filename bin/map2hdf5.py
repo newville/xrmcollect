c3266f7bb9c09ed0bcb647a86d5c816341bec5f7
Matt Newville <newville@cars.uchicago.edu>
2011-11-11 11:26:26 -0600

    added bin/map2hdf5.py

--- bin/map2hdf5.py
+++ bin/map2hdf5.py
@@ -1,9 +1,7 @@
-from lib.io import H5Writer
+from epicscollect.io import H5Writer
 import sys
 
 for dirname in sys.argv[1:]:
-    print ' ... ', dirname
-    
     writer = H5Writer(folder=dirname)
     writer.process()
 
