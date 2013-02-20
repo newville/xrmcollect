ae1dee0e5732894cd36b182de36e082233c72ced
Matt Newville <newville@cars.uchicago.edu>
2013-02-19 20:18:34 -0600

    qxafs: allowing uploading externally generated trajectory file

--- lib/qxafs.py
+++ lib/qxafs.py
@@ -27,28 +27,12 @@ from cStringIO import StringIO
 from string import printable
 from XPS_C8_drivers import  XPS
 
-opts, args = getopt.getopt(sys.argv[1:], "n:t:", ['npulses=','time='])
-
-if len(args) < 2:
-    print msg
-    sys.exit()
-
-energy1, energy2 = float(args[0]), float(args[1])
-print opts
-
-for key, val in opts:
-    if key in ('-n', '--npulses'):
-        npulses = int(val)
-    elif key in ('-t', '--time'):
-        dwelltime = float(val)
-
 
 RAD2DEG = 180/np.pi
 HC      = 12398.417
 def en2angle(energy, dspace):
     omega   = HC/(2.0 * dspace)
     return RAD2DEG * np.arcsin(omega/energy)
-
 
 
 class QXAFS_XPS:
@@ -64,17 +48,23 @@ class QXAFS_XPS:
     gather_outputs = ['MONO.THETA.SetpointPosition',
                       'MONO.THETA.CurrentPosition',
                       'MONO.THETA.FollowingError',
+                      'MONO.THETA.SetpointVelocity',                      
                       'MONO.THETA.CurrentVelocity',                      
+                      'MONO.THETA.SetpointAcceleration',                      
+                      'MONO.THETA.CurrentAcceleration',                      
                       'MONO.HEIGHT.CurrentPosition',
                       ]
     
-    def __init__(self, mono_pv='13IDA:m65',  energy_pv='13IDE:En:'):
+    def __init__(self, mono_pv='13IDA:m65', 
+                 energy_pv='13IDE:En:', use_undulator=True):
         self.xps = XPS()
         self.mono =  epics.Motor(mono_pv)
+        self.use_undulator = use_undulator
         self.dspace_pv = epics.PV("%sdspace" % energy_pv)
         self.traj = ''
         self.backup_angle = 0 # angle scanned in ramp-up portion of trajectory
         self.connect_xps()
+        self.nsegments = 3
         self.struck = Struck('13IDE:SIS1:', scaler='13IDE:scaler1')
         for i in range(8):
             s = self.struck.get('mca%i' % (i+1))
@@ -119,23 +109,36 @@ class QXAFS_XPS:
             "%f, %f, %f, %f, %f" % (dwelltime, xd_line,     xvelo, yd_line, yvelo),
             "%f, %f, %f, %f, %f" % (ramp_time, xd_ramp,         0, yd_ramp,     0),
             ]
-        self.traj = '\n'.join(traj)
+        self.traj = traj
         self.backup_angle = xd_ramp
 
+    def read_trajectory_file(self, fname):
+        f = open(fname, 'r')
+        self.traj = f.readlines()
+        dtime = 0
+        self.backup_angle = 0
+        for line in self.traj:
+            vals = [float(w) for w in line.split(',')]
+            if self.backup_angle == 0:
+                self.backup_angle = vals[1]
+            dtime = dtime + vals[0]
+        self.dwelltime = dtime
+        print 'Read Trajectory ', len(self.traj), self.dwelltime, self.backup_angle
+        self.start_angle = self.start_angle - self.backup_angle
+        print 'start angle = ', self.start_angle
+
     def upload_trajectory(self):
+
+        text = StringIO('\n'.join(self.traj))
+        self.nsegments = len(self.traj)
+
         ftpconn = ftplib.FTP()
         ftpconn.connect(self.host)
         ftpconn.login(self.username, self.password)
         ftpconn.cwd(self.traj_folder)
-
-        ftpconn.storbinary('STOR %s' % self.traj_name, StringIO(self.traj))
-
+        ftpconn.storbinary('STOR %s' % self.traj_name, text)
         ftpconn.close()
         print 'uploaded trajectories'
-        print '## qxafs.trj'
-        print self.traj
-        print '##'
-        ftpconn.close()
 
     def check_return(self, cmd, ret):
         if ret[0] != 0:
@@ -188,19 +191,21 @@ class QXAFS_XPS:
             self.gather = self.gather.replace(x,' ')
 
 
-    def save_gathering(self, fname='qxafs_xps.000'):
-        fname = increment_filename(fname)
-        f = open(fname, 'w')
+    def save_gathering(self, fname='qxafs_xps'):
+        gname = "%s_xps.000" % fname
+        sname = "%s_struck.000" % fname
+        gname = increment_filename(gname)
+        sname = increment_filename(sname)
+        f = open(gname, 'w')
         f.write("# QXAFS Data saved %s\n" % time.ctime())
         f.write("#-------------------\n")
         f.write("# %s\n" % ' '.join(self.gather_outputs))
         f.write(self.gather)
         f.close()
-        sfname = fname.replace('_xps', '_struck')
         time.sleep(0.1)
-        self.struck.saveMCAdata(fname = sfname)
-        print  'Saved data: ', fname, sfname
-
+        self.struck.saveMCAdata(fname = sname)
+        print  'Saved data: ', gname, ' / ', sname
+        return gname
 
     def build_scan(self, energy1, energy2, dtime=2.0, npulses=1001):
         dspace = self.dspace_pv.get()
@@ -209,30 +214,32 @@ class QXAFS_XPS:
         da = int(100*(a1- a2))/100.0
         self.energy1 = energy1
         self.energy2 = energy2
-        span = a2 - a1
         self.start_angle = a1
         self.npulses = npulses + 1
         self.dwelltime = dtime
-        self.create_trajectory(dwelltime=dtime, span=span)
+        self.create_trajectory(dwelltime=dtime, span=(a2-a1))
         self.upload_trajectory()
         self.start_angle = a1 - self.backup_angle
         print 'Built Scan OK'
-        self.mono_energies = np.linspace(energy1, energy2, npulses)
-        self.id_offset = epics.caget('13IDE:En:id_off')
 
     def onStruckPulse(self, pvname=None, value=0, **kws):
         if value % 25 == 0:
             self.new_id_en = value
             # print 'STruck value ', value
 
-    def prepare_scan(self):
+    def prepare_scan(self, npulses=None):
         """ put xps in Ready for Scan mode"""
         self.clear_xps_events()
 
-        self.set_undulator_scan()
+        if npulses is not None:
+            self.npulses = npulses
+        if self.use_undulator:
+            self.set_undulator_scan()
+
         dt = self.dwelltime / (self.npulses-1)
         ret = self.xps.GatheringReset(self.sid)
-        ret = self.xps.MultipleAxesPVTPulseOutputSet(self.sid, self.group_name,  1, 3, dt)
+        ret = self.xps.MultipleAxesPVTPulseOutputSet(self.sid, 
+                              self.group_name,  1, self.nsegments, dt)
 
         self.check_return('MultipleAxesPVTPulseOutputSet', ret)
 
@@ -266,19 +273,16 @@ class QXAFS_XPS:
         print 'Scan Prepare OK'
 
     def set_undulator_scan(self):
-        print 'setup undulator scan'
         self.id_dat = np.loadtxt('Harmonic1.dat').transpose()
         en = self.id_dat[0]
         gap = self.id_dat[1]
-        # self.mono_energies = np.linspace(energy1, energy2, npulses)
-        # self.id_offset = epics.caget('13IDE:En:id_off')
 
         gap1 = np.interp(self.energy1, en, gap)
         gap2 = np.interp(self.energy2, en, gap)
+
         epics.caput('ID13us:SSStartGap', gap1)
         epics.caput('ID13us:SSEndGap', gap2)
         epics.caput('ID13us:SSTime', self.dwelltime)
-        print 'Set SSTime to ', self.dwelltime
 
     def execute_trajectory(self):
         return self.xps.MultipleAxesPVTExecution(self.sid, self.group_name, 
@@ -298,19 +302,24 @@ class QXAFS_XPS:
     def run(self):
         """run traj"""
         print 'Run Trajectory'
-        self.new_id_en = None
-        id_offset = epics.caget('13IDE:En:id_off')
-        id1 = self.energy1/1000.0 + id_offset 
-        id2 = self.energy2/1000.0 + id_offset 
+        if self.use_undulator:
+            id_offset = epics.caget('13IDE:En:id_off')
+            id1 = self.energy1/1000.0 + id_offset 
+            id2 = self.energy2/1000.0 + id_offset 
         self.mono.move(self.start_angle)
-        epics.caput('13IDE:En:id_track', 0)
-        epics.caput('ID13us:ScanEnergy', id1)
+        
+        if self.use_undulator:
+            epics.caput('13IDE:En:id_track', 0)
+            epics.caput('ID13us:ScanEnergy', id1)
+
         struck_pv = epics.PV('13IDE:SIS1:CurrentChannel')
         struck_pv.add_callback(self.onStruckPulse)
 
         self.mono.move(self.start_angle, wait=True)
-
-        epics.caput('ID13us:SyncScanMode', 1)
+        
+        
+        if self.use_undulator:
+            epics.caput('ID13us:SyncScanMode', 1)
         print 'Before Trajectory: '
         print '   Angle  = ', epics.caget('13IDA:m65.VAL'), epics.caget('13IDA:m65.DVAL')
         print '   Energy = ', epics.caget('13IDE:En:Energy')
@@ -326,25 +335,16 @@ class QXAFS_XPS:
 
         scan_thread = Thread(target=self.execute_trajectory)
 
-        epics.caput('ID13us:SSStart', 1)
-        print 'Waiting for Undulator Sync'
-        while True:
-            if epics.caget('ID13us:SSState') == 2:
-                break
+        if self.use_undulator:
+            epics.caput('ID13us:SSStart', 1)
+            print 'Waiting for Undulator Sync'
+            while True:
+                if epics.caget('ID13us:SSState') == 2:
+                    break
         self.struck.start()
-        print 'ID scanning, starting trajectory'
         scan_thread.start()
-        #moved_id = False
-        #while scan_thread.isAlive():
-            #if self.new_id_en is not None and not moved_id:
-            #    epics.caput('ID13us:ScanEnergy', id2)
-            #    moved_id = True
-            # mon_en = self.mono_energies[self.new_id_en]
-            # id_gap = np.interp(mon_en, table_mono_en, table_id_gap)
-            # epics.caput('ID13us:ScanGap', id_gap)
-            # print 'set ID Scan Energy ', self.new_id_en, id_gap
-            # self.new_id_en = None
-            # time.sleep(0.01)
+        print 'ID scanning, started trajectory'
+
         scan_thread.join()
         print 'Trajectory Thread Done!'
 
@@ -353,17 +353,36 @@ class QXAFS_XPS:
 
         self.check_return('GatheringStop', ret)
         self.struck.stop()
-        epics.caput('ID13us:SyncScanMode', 0)
+        if self.use_undulator:
+            epics.caput('ID13us:SyncScanMode', 0)
         
 
-q = QXAFS_XPS(mono_pv='13IDA:m65',  energy_pv='13IDE:En:')
-q.build_scan(energy1, energy2, dtime=dwelltime, npulses=npulses)
-start_time = time.time()
+if __name__ == '__main__':
 
-q.prepare_scan()
-q.run()
-q.read_gathering()
-q.save_gathering()
+    opts, args = getopt.getopt(sys.argv[1:], "n:t:", ['npulses=','time='])
 
-print 'Done: time = %.2f sec ' % (time.time()-start_time)
+    if len(args) < 2:
+        print msg
+        sys.exit()
+        
+    energy1, energy2 = float(args[0]), float(args[1])
+    for key, val in opts:
+        if key in ('-n', '--npulses'):
+            npulses = int(val)
+        elif key in ('-t', '--time'):
+            dwelltime = float(val)
+
+
+    q = QXAFS_XPS(mono_pv='13IDA:m65',  energy_pv='13IDE:En:', use_undulator=False)
+    q.build_scan(energy1, energy2, dtime=dwelltime, npulses=npulses)
+    start_time = time.time()
+
+    print 'Traj: '
+    print q.traj
+    q.prepare_scan()
+    q.run()
+    q.read_gathering()
+    q.save_gathering()
+    
+    print 'Done: time = %.2f sec ' % (time.time()-start_time)
 
