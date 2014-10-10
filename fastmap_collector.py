715d698b7c3e2cf6d0f636004a230d6dbc838496
Matt Newville <newville@cars.uchicago.edu>
2014-10-10 06:55:53 -0500

    add command-line option to fastmap_collector

--- fastmap_collector.py
+++ fastmap_collector.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python
 
-# from epicscollect import collector 
-from lib import collector 
+# from epicscollect import collector
+from lib import collector
+from optparse import OptionParser
 
-t = collector.TrajectoryScan()
+usage = "usage: %prog [options] file(s)"
+parser = OptionParser(usage=usage, prog="fastmap_collector",
+                      version="larch command-line version 0.2")
+
+parser.add_option("-x", "--xrf", dest="xrf_prefix", action="store_true",
+                  default='13SDD1:',
+                  help="set xrf_prefix, default = 13SDD1:")
+
+
+(opts, args) = parser.parse_args()
+
+t = collector.TrajectoryScan(xrf_prefix=opts.xrf_prefix)
 t.mainloop()
