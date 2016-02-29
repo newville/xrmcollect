4367ae9fcb87f91e3dedb0891f72c15ff40526bc
Matt Newville <newville@cars.uchicago.edu>
2016-02-29 16:06:12 -0600

    new set mono tilt

--- lib/set_mono_tilt.py
+++ lib/set_mono_tilt.py
@@ -1,97 +1,111 @@
 import time
 from epics import caget, caput
 
-i0_pv    = '13IDE:IP330_1.VAL'
-tilt_pv  = '13IDA:DAC1_7.VAL'
-roll_pv  = '13IDA:DAC1_8.VAL'
-ts_pv    = '13XRM:edb:arg01'
+def find_max_intensity(readpv, drivepv, vals, minval=0.1):
+    """find a max in an intensity while sweeping through an
+    array of drive values,  around a current position, and
+    move to the position with max intensity.
 
-def find_peak_intensity(ctrl_pv, read_pv, delay_time=0.10, 
-                        delta=0.025, npoints=50, minval=0.1):
+    Parameters
+    ----------
+    readpv:   PV for reading intensity
+    drivepv:  PV for driving positions
+    vals:     array of RELATIVE positions (from current value)
+    minval:   minimum acceptable intensity [defualt = 0.1]
 
+    Notes:
+    -------
+     1. PRIVATE method, not exposed in user-macros
+     2. if the best intensity is below minval, the position is
+        moved back to the original position.
+     
     """
-    find value for ctrl pv (ie, DAC) around current value
-    that gives max intensity in read_pv
-    """
-    
-    time.sleep(delay_time)
-    
-    ctrl_best = ctrl_orig = caget(ctrl_pv)
-    read_best = caget(read_pv)
-    for i in range(2*npoints):
-        cval =  ctrl_orig + (i-npoints) * delta
-        caput(ctrl_pv, cval)
-        time.sleep(delay_time)
-        if i == 0:
-            time.sleep(delay_time)
-            
-        read_val = caget(read_pv)
-        if read_val > read_best:
-            read_best = read_val
-            ctrl_best = cval
-    
-    if read_best < minval:
-        ctrl_best = ctrl_orig
-    print 'Peak:  %s=%f at %s=%f' % (read_pv, read_best,
-                                     ctrl_pv, ctrl_best)
-    
-    caput(ctrl_pv, ctrl_best)
+    _orig = _best = caget(drivepv)
+    i0max = caget(readpv)
+    for val in _orig+vals:
+        caput(drivepv, val)
+        sleep(0.1)
+        i0 = caget(readpv)
+        if i0 > i0max:
+            i0max, _best = i0, val
+        #endif
+    #endfor
+    if i0max < minval: _best = _orig
+    caput(drivepv, _best)
+    return i0max, _best
+#enddef
 
-def set_mono_tilt(use_bpm=False, do_roll=True):
-    # adjust IDE mono tilt and roll DAC to maximize mono pitch
-    print 'SET MONO TILT  using bpm = ', use_bpm, ' doing roll = ', do_roll
-    xt0 = time.time()
+def set_mono_tilt(enable_fb_roll=True, enable_fb_pitch=False):
+    """Adjust IDE monochromator 2nd crystal tilt and roll
+    to maximize intensity.
+
+    Parameters
+    ----------
+    enable_fb_roll:  True (default) or False:
+                     enable roll feedback after best position is found.
+    enable_fb_pitch: True or False (default):
+                     enable pitch feedback after best position is found.
+
+    Notes:
+    -------
+     This works by
+        1. adjusting pitch to maximize intensity at BPM
+        2. adjusting roll to maximize intensity at I0 Ion Chamber
+        3. adjusting pitch to maximize intensity at I0 Ion Chamber
+    """
+
+    print 'Set Mono Tilt June 2015'
+    with_roll = True
     tilt_pv = '13IDA:DAC1_7.VAL'
     roll_pv = '13IDA:DAC1_8.VAL'
     i0_pv   = '13IDE:IP330_1.VAL'
+    sum_pv  = '13IDA:QE2:SumAll:MeanValue_RBV'
 
+    caput('13XRM:edb:use_fb', 0)
     caput('13IDA:efast_pitch_pid.FBON', 0)
     caput('13IDA:efast_roll_pid.FBON', 0)
-    if use_bpm:
-        i0_pv = '13IDA:QE2:Sum1234:MeanValue_RBV'
 
     i0_minval = 0.1   # expected smallest I0 Voltage
-    dac_delta = 0.025 # min step in DAC Voltage
-    npoints   = 40    # max number of DAC steps +/- current position
 
-    # find best tilt value
-    tilt_best = tilt_orig = caget(tilt_pv) 
-    i0_best = caget(i0_pv)
+    # stop, restart Quad Electrometer
+    caput('13IDA:QE2:Acquire', 0) ;     sleep(0.25)
+    caput('13IDA:QE2:Acquire', 1) ;     sleep(0.25)
+    caput('13IDA:QE2:ReadData.PROC', 1)
 
-    for i in range(2*npoints):
-        tval =  tilt_orig + (i-npoints) * dac_delta
-        caput(tilt_pv, tval)
-        time.sleep(0.10)
-        i0 = caget(i0_pv)
-        if i0 > i0_best:
-            i0_best, tilt_best = i0, tval
+    # find best tilt value with BPM sum
+    out = find_max_intensity(sum_pv, tilt_pv, linspace(-2.5, 2.5, 101))
+    print 'Best Pitch (BPM): %.3f at %.3f ' % (out)
+    sleep(0.5)
 
-    if i0_best < i0_minval:
-        tilt_best = tilt_orig
-    caput(tilt_pv, tilt_best)
+    # find best tilt value with IO
+    out = find_max_intensity(i0_pv, tilt_pv, linspace(-1.0, 1.0, 51))
+    print 'Best Pitch (I0): %.3f at %.3f ' % (out)
+    sleep(0.25)
 
-    if not do_roll:
-        return
-    roll_best = roll_orig = caget(roll_pv)
-    i0_best = caget(i0_pv)
-    for i in range(2*npoints):
-        tval =  roll_orig + (i-npoints) * dac_delta
-        caput(roll_pv, tval)
-        time.sleep(0.10)
-        i0  = caget(i0_pv)
-        if i0 > i0_best:
-            i0_best, roll_best = i0, tval
+    # find best roll with I0
+    if with_roll:
+        print 'doing roll..'
+        out = find_max_intensity(i0_pv, roll_pv, linspace(-3.5, 3.5, 141))
+        print 'roll first pass ', out
+        if out[0] > 0.002:
+            out = find_max_intensity(i0_pv, roll_pv, linspace(0.75 -0.75, 76))
+        #endif
+        print 'Best Roll %.3f at %.3f ' % (out)
+        sleep(0.25)
+    #endif
 
-    if i0_best < i0_minval:
-        roll_best = roll_orig
-    caput(roll_pv, roll_best)
-
-    time.sleep(2.0)
-    caput('13IDA:QE2:ComputePosOffset12.PROC', 1)
-    caput('13IDA:QE2:ComputePosOffset34.PROC', 1)
-    caput('13IDA:efast_roll_pid.FBON', 0)
+    # re-find best tilt value, now using I0
+    out = find_max_intensity(i0_pv, tilt_pv, linspace(-1, 1, 51))
+    print 'Best Pitch: %.3f at %.3f ' % (out)
+    sleep(1.0)
+    caput('13IDA:QE2:ComputePosOffsetX.PROC', 1, wait=True)
+    caput('13IDA:QE2:ComputePosOffsetY.PROC', 1, wait=True)
+    sleep(0.5)
     caput('13IDA:efast_pitch_pid.FBON', 0)
-    # print 'Set Mono Tilt Time : %.3f' % (time.time()-xt0)
+    if enable_fb_roll:
+        caput('13IDA:efast_roll_pid.FBON', 1)
+    if enable_fb_pitch:
+        caput('13XRM:edb:use_fb', 1)
+    
+#enddef
 
-if __name__ == '__main__':
-    set_mono_tilt(do_roll=True)
